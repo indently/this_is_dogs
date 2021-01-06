e31b89bb3c236ec517a1a6be96527dd702f20f56
Federico <federicosou@fastmail.se>
2021-01-06 10:26:46 +0100

    Added some documentation

--- app/src/main/java/com/codepalace/apistuff/MainActivity.kt
+++ app/src/main/java/com/codepalace/apistuff/MainActivity.kt
@@ -28,17 +28,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Keeps the phone in light mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        // Starts up the background transitions
         backgroundAnimation()
 
+        // Makes an API request as soon as the app starts
         makeApiRequest()
 
         binding.floatingActionButton.setOnClickListener {
 
+            // FAB rotate animation
             binding.floatingActionButton.animate().apply {
                 rotationBy(360f)
-                duration =  1000
+                duration = 1000
             }.start()
 
             makeApiRequest()
@@ -57,18 +61,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeApiRequest() {
-
         val api = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiRequest::class.java)
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApiRequest::class.java)
 
         GlobalScope.launch(Dispatchers.IO) {
             try {
                 val response = api.getRandomDog()
                 Log.d("Main", "Size: ${response.fileSizeBytes}")
 
+                //If the image is less than about 3mb, then we try to load it into our app, else we try again.
                 if (response.fileSizeBytes < 300_000) {
                     withContext(Dispatchers.Main) {
                         Glide.with(applicationContext).load(response.url).into(binding.ivRandomDog)
